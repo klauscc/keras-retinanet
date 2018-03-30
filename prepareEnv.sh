60c7737ba33d134a63339fe75296a9252261594a
klaus <klaus.cheng@qq.com>
2018-03-30 03:42:49 -0400

    add cocoapi

---
+++ prepareEnv.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+export PYTHONPATH="`pwd`/cocoApi/:`pwd`:$PYTHONPATH"
+DATA_DIR=/data/users/$USER/004-nsp
+mkdir -p $DATA_DIR
+if [ ! -L data ]; then
+    ln -s "$DATA_DIR" data
+fi
